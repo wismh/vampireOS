af7aa5737f69398b83442d5d4fd84b886e5243fd
wismh <68060501+wismh@users.noreply.github.com>
2026-02-18 18:05:00 +0300

    vos-64 feat: list the cwd from ring 3

---
+++ user/ls.asm
@@ -0,0 +1,64 @@
+; BASE 0x400000; readdir cwd names, write fd 1, exit
+bits 64
+org 0x400000
+
+ehdr:
+    db 0x7F, "ELF", 2, 1, 1, 0
+    times 8 db 0
+    dw 2
+    dw 62
+    dd 1
+    dq _start
+    dq phdr - $$
+    dq 0
+    dd 0
+    dw 64
+    dw 56
+    dw 1
+    dw 0
+    dw 0
+    dw 0
+
+phdr:
+    dd 1
+    dd 7
+    dq 0
+    dq $$
+    dq $$
+    dq filesize
+    dq filesize
+    dq 0x1000
+
+_start:
+    mov eax, 9
+    mov edi, buf
+    mov esi, 256
+    int 0x30
+    test rax, rax
+    js fail
+    mov ecx, eax
+    test ecx, ecx
+    jz done
+    mov eax, 1
+    mov edi, 1
+    mov esi, buf
+    mov edx, ecx
+    int 0x30
+done:
+    mov eax, 2
+    xor edi, edi
+    int 0x30
+fail:
+    mov eax, 1
+    mov edi, bad
+    int 0x30
+    mov eax, 2
+    xor edi, edi
+    int 0x30
+
+bad:
+    db "X", 0
+buf:
+    times 256 db 0
+
+filesize equ $ - $$
